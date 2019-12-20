7d158bf0c1c7c2dcb7fb9c3b6861194c0e0374ad
Lucas Yang <yangchenshin77@gmail.com>
2019-12-20 22:10:29 +0800

    Update TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,8 @@
 
 namespace Ycs77\LaravelLineBot\Test;
 
-use Mockery;
+use Closure;
+use Mockery as m;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Ycs77\LaravelLineBot\LineBotServiceProvider;
 
@@ -23,14 +24,40 @@ class TestCase extends OrchestraTestCase
     }
 
     /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('linebot.channel_access_token', 'Foo');
+        $app['config']->set('linebot.channel_secret', 'Bar');
+        $app['config']->set('linebot.endpoint_base', 'https://api.line.me');
+        $app['config']->set('linebot.data_endpoint_base', 'https://api-data.line.me');
+    }
+
+    /**
      * Mock an instance of an object in the container.
      *
      * @param  string  $abstract
      * @param  \Closure|null  $mock
-     * @return object
+     * @return \Mockery\MockInterface
      */
-    protected function mock($abstract, $mock = null)
+    protected function mock($abstract, Closure $mock = null)
     {
-        return $this->app->instance($abstract, Mockery::mock(...array_filter(func_get_args())));
+        return $this->app->instance($abstract, m::mock(...array_filter(func_get_args())));
+    }
+
+    /**
+     * Mock a partial instance of an object in the container.
+     *
+     * @param  string  $abstract
+     * @param  \Closure|null  $mock
+     * @return \Mockery\MockInterface
+     */
+    protected function partialMock($abstract, Closure $mock = null)
+    {
+        return $this->app->instance($abstract, m::mock(...array_filter(func_get_args()))->makePartial());
     }
 }
